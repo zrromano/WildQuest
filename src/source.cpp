8b6e04bfac0a58fb6a99ae6a3fd6d67f2587a9d9
Zachary Romano <zrromano@mavs.coloradomesa.edu>
2019-03-01 09:46:03 -0700

    Projectiles working

--- src/source.cpp
+++ src/source.cpp
@@ -131,14 +131,14 @@ class Animation{
 	vector < frameWithTime > frames;
 	int time, totalTime, frame; //in ms
 	public:
-	Animation(Game *game, string path, int count, int fps, int x, int y) {
+	Animation(Game *game, string path, int count, int fps, int spriteX=0, int spriteY=0) {
 		stringstream filename;
 		//path images/frame i .bmp
 		totalTime = 0;
 		for(int i=0; i < count; i++) {
 			filename.str("");
 			filename << path << setw(4) << setfill('0') << i << ".bmp";
-			Image *img = new Image(game, filename.str(), x, y);
+			Image *img = new Image(game, filename.str(), spriteX, spriteY);
 			frames.push_back(frameWithTime(img,1000/fps));
 			totalTime += 1000;
 		}
@@ -179,18 +179,29 @@ class Animation{
 class Sprite:public Animation {
 	float x,y,dx,dy,ax,ay;
 	int gw,gh;
+	bool dead;
 	public:
-	Sprite(Game *g, string filename, int count=1, int fps=30, float _x=0.0, float _y=0.0, float _dx=0.0, float _dy=0.0, float _ax=0.0, float _ay=0.0):
-	  Animation(g, filename, count, fps, _x, _y){
+	bool within(float otherX, float otherY){
+		SDL_Rect us = getSize();
+		return x<=otherX && otherX<=x+us.w && y<=otherY && otherY<=y+us.h;
+	}
+	bool collidesWith(Sprite &other){
+		SDL_Rect them = other.getSize();
+		return within(other.x, other.y) || 
+				within(other.x + them.w, other.y) ||
+				within(other.x + them.w, other.y + them.h) ||
+				within(other.x, other.y + them.h);
+	}
+	Sprite(Game *g, string filename, int count=1, int fps=30, float _x=0.0, float _y=0.0, float _dx=0.0, float _dy=0.0, float spriteX=0.0, float spriteY=0.0):
+	  Animation(g, filename, count, fps, spriteX, spriteY){
 		x = _x;
 		y = _y;
 		dx = _dx;
 		dy = _dy;
-		ax = _ax;
-		ay = _ay;
 		SDL_Rect resolution = g->getResolution();
 		gw = resolution.w;
 		gh = resolution.h;
+		dead = false;
 	}
 	void random() {
 		x=(float)(rand() % gw);
@@ -199,10 +210,9 @@ class Sprite:public Animation {
 		dy=(float)((rand()% (40*100))-2000)/100.0;
 	}
 	void update(float dt /* in ms */, int setFrame=-1){
-		Animation::update(dt, setFrame);
+		if(!dead){
+		//Animation::update(dt, setFrame);
 		dt/=1000.0;
-		dx = dx + ax * dt;
-		dy = dy + ay * dt;
 		x = x + dx * dt;
 		y = y + dy * dt;
 		SDL_Rect src = getSize();
@@ -226,6 +236,8 @@ class Sprite:public Animation {
 			dx = 0;
 			x = gw-src.w;
 		}
+		}
+		else {delete this;}
 	}
 	void Render(Game *g, int setX=-1, int setY=-1){
 		if(setX != -1 && setY != -1) {
@@ -246,16 +258,21 @@ class Sprite:public Animation {
 	void setDy(float _dy){ dy = _dy; }
 	float getDx(){ return dx; }
 	float getDy(){ return dy; }
+	float getX() { return x; }
+	float getY() { return y; }
 };
 
 class MyGame:public Game {
 	Image *background;
 	Sprite *player;
+	vector<Sprite *> projectiles;
 	public:
 	MyGame():Game("Wild Quest"){};
     void init(){
 		background = new Image(this, "../res/back.bmp");
 		player = new Sprite(this, "../res/playerSprite", 2, 1);
+		Sprite *projectile = new Sprite(this,"../res/bullet", 1, 30, player->getX(), 500.0,500.0);
+		projectiles.push_back(projectile);
 	}
 	//Game loop, basic gameplay functionality goes here
 	/*void loop(){
@@ -358,17 +375,40 @@ class MyGame:public Game {
 		}
 		else if(player->getDy() > 0)	
 			player->accelerateY(-5);
+			
+		if(keystate[SDL_SCANCODE_RIGHT]){
+			player->setFrame(0);
+			Sprite *projectile = new Sprite(this,"../res/bullet", 1, 30, player->getX(), player->getY(), (player->getDx()/2 + 400), player->getDy());
+			projectiles.push_back(projectile);
+		}
+		if(keystate[SDL_SCANCODE_LEFT]){
+			player->setFrame(1);
+			Sprite *projectile = new Sprite(this,"../res/bullet", 1, 30, player->getX(), player->getY(), (player->getDx()/2 - 400), player->getDy());
+			projectiles.push_back(projectile);
+		}
+		if(keystate[SDL_SCANCODE_DOWN]){
+			Sprite *projectile = new Sprite(this,"../res/bullet", 1, 30, player->getX(), player->getY(), player->getDx(), (player->getDy()/2 + 400));
+			projectiles.push_back(projectile);
+		}
+		if(keystate[SDL_SCANCODE_UP]){
+			Sprite *projectile = new Sprite(this,"../res/bullet", 1, 30, player->getX(), player->getY(), player->getDx(), (player->getDy()/2 - 400));
+			projectiles.push_back(projectile);
+		}
 	}
 	
 	void update(float dt){
 		//cout << dt << endl;
 		player->update(dt, player->getFrame());
+		for(int i = 0; i < projectiles.size(); i++)
+			projectiles[i]->update(dt,projectiles[i]->getFrame());
 	}
 	
 	void Render(){
 		SDL_Renderer *renderer = getRenderer();
 		background->Render(this);
 		player->Render(this);
+		for(int i = 0; i < projectiles.size(); i++)
+			projectiles[i]->Render(this);
 		SDL_RenderPresent(renderer);
 	}	
 };
