f686c4a3f5805ef80f46e1478ba380ee54685829
Dustin Reynolds <DustinLeprechaun@gmail.com>
2019-04-09 19:45:35 -0600

    Created new collision check method

---
+++ src/Utilities.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+using namespace std;
+
+bool CollisionCheck(SDL_Rect a, SDL_Rect b)
+{
+    //rectange sides
+    int leftA, leftB;
+    int rightA, rightB;
+    int topA, topB;
+    int bottomA, bottomB;
+
+    leftA = a.x;
+    rightA = a.x + a.w;
+    topA = a.y;
+    bottomA = a.y + a.h;
+
+    leftB = b.x;
+    rightB = b.x + b.w;
+    topB = b.y;
+    bottomB = b.y + b.h;
+
+    //If any of the sides from A are outside of B
+    if( bottomA <= topB ) return false;
+    if( topA >= bottomB ) return false;
+    if( rightA <= leftB ) return false;
+    if( leftA >= rightB ) return false;
+
+    //If none of the sides from A are outside B
+    return true;
+}
